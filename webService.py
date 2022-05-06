7e252b96e0f6392e6bd5c2e7d06668557b570b2b
An-225 <66622436+An-225@users.noreply.github.com>
2022-05-06 18:46:49 -0300

    Basic log configuration

--- webService.py
+++ webService.py
@@ -1,6 +1,10 @@
 #import dataService
-
+import logging
+from turtle import st
 from flask import Flask
+
+logging.basicConfig(filename='logs.log', level=logging.DEBUG)
+
 app = Flask(__name__)
 app.run(debug=True)
 
@@ -9,19 +13,23 @@ tokens = ["abadhasj4343nujdas", "asjdbasdsjkll345678*skad", "ansjdansjdas-89/*ad
 @app.route('/deposito/<acnt>/<amt>',methods=['GET'])
 def deposito(acnt, amt):
     str = 'deposito de ' + acnt + ' para' + amt
+    logging.info(str);
     return str
 
 @app.route('/saque/<acnt>/<amt>',methods=['GET'])
 def saque(acnt, amt):
     str = 'saque de ' + acnt + ' para' + amt
+    logging.info(str);
     return str
 
 @app.route('/saldo/<acnt>',methods=['GET'])
 def saldo(acnt):
     str = 'Saldo de ' + acnt
+    logging.info(str);
     return str
 
 @app.route('/transferencia/<acnt_orig>/<acnt_dest>/<amt>',methods=['GET'])
 def transferencia(acnt_orig, acnt_dest,  amt):
     str = 'transferencia de ' + acnt_orig + " para " + acnt_dest + " no valor de " + amt
+    logging.info(str);
     return str
